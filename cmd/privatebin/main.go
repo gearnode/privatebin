ee07c989cd9d081a9068ef3a6c2bf0960f6968c7
gearnode <bryan@frimin.fr>
2022-11-06 13:38:13 +0100

    Enforce cmd vanity import

--- cmd/privatebin/main.go
+++ cmd/privatebin/main.go
@@ -14,7 +14,7 @@
 // TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 // PERFORMANCE OF THIS SOFTWARE.
 
-package main
+package main // import "gearno.de/cmd/privatebin"
 
 import (
 	"bufio"
